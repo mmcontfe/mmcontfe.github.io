b481def3bd6c1564a50713193213747e14ba261a
孟世博 <897205285@qq.com>
2022-09-16 14:18:08 +0800

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5dd641c8893d9415"; }
+/******/ 		__webpack_require__.h = function() { return "5d5c8d3c91300856"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/posts/index.md
+++ pages/posts/index.md
@@ -1,6 +1,6 @@
 ---
 type: posts
-title: Posts
+title: 文章
 date: 2021-03-18
 ---
 
