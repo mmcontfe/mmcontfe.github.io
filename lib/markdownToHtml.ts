f0e5934b7dd209e21ff8993606c51c3afbbe1e49
孟世博 <897205285@qq.com>
2022-09-20 11:53:03 +0800

    feat: use rehype

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,22 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+
+import { unified } from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeDocument from 'rehype-document'
+import rehypeFormat from 'rehype-format'
+import rehypeStringify from 'rehype-stringify'
+import rehypeHighlight from 'rehype-highlight'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
-  return result.toString()
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeDocument)
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .use(rehypeHighlight)
+    .process(markdown)
+
+  return String(file)
+
 }
